b6302450f28864db7145b48182409dd5be3c7264
snehaljha <snehaljha08@gmail.com>
2023-04-15 23:07:10 +0530

    add custom header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector:'notes-header',
@@ -6,6 +6,9 @@ import { Component } from "@angular/core";
     styleUrls:['header.component.css']
 })
 export class HeaderComponent{
+
+    @Input() name: string | undefined;
+
     title:string = 'Notes!!'
 
 }
